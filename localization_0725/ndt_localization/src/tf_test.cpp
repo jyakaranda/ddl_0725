82723919c29c053a68df0be075e51172e21c8b31
jyakaranda <1217358645@qq.com>
2018-10-16 17:18:22 +0800

    test bug in libwaypoint_follower of Autoware

--- localization_0725/ndt_localization/src/tf_test.cpp
+++ localization_0725/ndt_localization/src/tf_test.cpp
@@ -26,6 +26,24 @@ int main(int argc, char **argv)
 
     ROS_INFO("yaw: %f, pitch: %f, roll: %f", yaw, pitch, roll);
     broadcaster.sendTransform(tf::StampedTransform(tf::Transform(q, tf::Vector3(0, 1, 0)), ros::Time::now(), "/map", "/odom"));
+    tf::Transform cur_pose;
+    q.setRPY(0., 0., 0.);
+    cur_pose.setRotation(q);
+    cur_pose.setOrigin(tf::Vector3(1., 1., 1.));
+    geometry_msgs::Point p = geometry_msgs::Point();
+    p.x = 3.;
+    p.y = 3.;
+    p.z = 3.;
+    tf::Point pose;
+    // pose.setRotation(q);
+    // pose.setOrigin(tf::Vector3(3., 3., 3.));
+    tf::pointMsgToTF(p, pose);
+
+    // broadcaster.sendTransform(tf::StampedTransform(cur_pose, ros::Time::now(), "/map", "/cur"));
+    // broadcaster.sendTransform(tf::StampedTransform(pose, ros::Time::now(), "/map", "/pose"));
+    // broadcaster.sendTransform(tf::StampedTransform(cur_pose * pose, ros::Time::now(), "/cur", "/pose"));
+    tf::Point r = cur_pose.inverse() * pose;
+    ROS_INFO("x: %f, y: %f, z: %f", r.getX(), r.getY(), r.getZ());
     d.sleep();
   }
 
